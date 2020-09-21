99298d7c27f605f8bc1082b2abccdd5e3fde0dbe
Natphatson <68212425+Natphatson@users.noreply.github.com>
2020-09-21 12:21:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_6115261018w02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
